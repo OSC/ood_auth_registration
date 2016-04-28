128d5a321df1e41738c86194cf432cbd7665f670
Eric Franz <efranz@osc.edu>
2016-04-27 23:11:38 -0400

    update success view

--- success.php
+++ success.php
@@ -1,1 +1,35 @@
-<h1>Success</h1>
+<!DOCTYPE html>
+<!--[if lt IE 9]><html class="lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--><html lang="en"><!--<![endif]-->
+<head>
+  <title>OnDemand | Register OpenID Provider</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="refresh" content="5;url=/">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+<style type="text/css">
+</style>
+<body>
+
+<!-- navigation bar -->
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+    </div>
+  </div><!-- /.navbar-collapse -->
+</nav>
+<!-- navbar -->
+
+
+<!-- content -->
+<div class="container">
+  <h3>Successfully registered provider with OSC account</h3>
+  <p class="lead">You should be redirected to your destination in 5 seconds. If not, <a href="/">click here to go to the dashboard.</a></p>
+</div>
+
+</body>
+</html>
