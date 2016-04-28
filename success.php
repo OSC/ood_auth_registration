bc4449823e756c3fd83cc612726af0d3b5dcc97e
Eric Franz <efranz@osc.edu>
2016-04-27 23:30:10 -0400

    add OD3 branding

--- success.php
+++ success.php
@@ -11,14 +11,27 @@
   <link rel="stylesheet" href="css/bootstrap.min.css">
 
 <style type="text/css">
+.navbar-brand {
+  color: white !important;
+}
+
+.navbar.navbar-inverse {
+  background-color: #cf102d !important;
+  border-color: #a00c23 !important;
+}
+
+body {
+  padding-top: 60px;
+}
 </style>
 <body>
 
 <!-- navigation bar -->
-<nav class="navbar navbar-default" role="navigation">
-  <div class="container">
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
+      <a class="navbar-brand" href="/">OSC OnDemand</a>
     </div>
   </div><!-- /.navbar-collapse -->
 </nav>
