96d0d8ee8cb04754c4cef33615ab29e1c7bf314b
Brian McMichael <bmcmichael@osc.edu>
2016-11-18 12:43:34 -0500

    create a fetch function

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
 require_once('ldap.php');
 
 /**
+ * Fetch a variable from an object without php warnings
+ *
+ * @param $source The array containing data
+ * @param $key The key that will be inspected
+ * @param null $default An optional return value if location is empty
+ * @return null Return the value of $source[$key] or null if empty
+ */
+function fetch($source, $key, $default = NULL) {
+    return isset($source[$key]) ? $source[$key] : $default;
+}
+
+/**
  * Ensures provided username is valid
  *
  * @param string $username  the provided username
